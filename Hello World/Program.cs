8a195875641ced089604cbc4e10c41a19d020365
Raine Taber <srt1849@rit.edu>
2022-01-14 15:34:10 -0500

    Hello world!

---
+++ Hello World/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Raine R. Taber");
+            String myName = "Raine R. Taber";
+            Console.WriteLine(myName + "" + 6);
+            for (int x = 0; x < 5; x++)
+            {
+                Console.WriteLine(myName + " " + x);
+                Console.WriteLine("nice to meet you!");
+            }
+        }
+    }
+}
