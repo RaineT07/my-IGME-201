d2ac577a9587afef1403a1f1fccfbd7e1c24c2ed
Raine Taber <srt1849@rit.edu>
2022-01-22 16:53:17 -0500

    PE4-6, mandelBrot

---
+++ PE4-q6/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE4_q6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //given variables initialized
+            double realCoord, imagCoord;
+            double realTemp, imagTemp, realTemp2, arg;
+            int iterations;
+
+            //my variables initialized
+            bool isValid = false;
+            double yStartInput = 1.2;
+            double yEndInput = -1.2;
+            double xStartInput = -0.6;
+            double xEndInput = 1.77;
+            //provide directions for input, and parse said input into yEndInput which is used
+            //for the second clause of the first for-loop. this is done in this order
+            //so instructions are easier for the user to follow while code is running.
+            Console.Write("Please enter the first value. default for this value is -1.2. ");
+            //parse user input for the yEndInput variable.
+            while (!isValid)
+            {
+                isValid = double.TryParse(Console.ReadLine(), out yEndInput);
+                if (!isValid) { Console.Write("Please input a number: "); }
+
+            }
+            isValid = false;
+            //provide directions for second input, which is directed into yStartInput, the first clause of the first for-loop
+            Console.Write("Please enter a second value larger than the first value. default for this value is 1.2. ");
+            //parse user input for the yStartInput variable
+            while (!isValid)
+            {
+                isValid = double.TryParse(Console.ReadLine(), out yStartInput);
+                if (!isValid || yStartInput < yEndInput) { Console.Write("Please input a valid number"); }
+            }
+            isValid = false;
+            //provide directions for third input, which is directed into xEndInput, the second clause of the second for-loop
+            Console.Write("Please enter your third value, default for this value is 1.77. ");
+            //parse user input for the xEndInput variable
+            while (!isValid)
+            {
+                isValid = double.TryParse(Console.ReadLine(), out xEndInput);
+                if (!isValid) { Console.Write("Please input a valid number"); }
+            }
+            isValid = false;
+            Console.Write("Please enter a fourth value smaller than the third value. default for this value is -0.6. ");
+            while (!isValid)
+            {
+                isValid = double.TryParse(Console.ReadLine(), out xStartInput);
+                if (!isValid || xStartInput > xEndInput) { Console.Write("Please input a valid number"); }
+            }
+
+            //calculate the third clause for both for loops such that there is still 48 values in the first for-loop
+            //and 80 values in the second for-loop
+            double yInterval = (Math.Abs(yStartInput) + Math.Abs(yEndInput)) / 48;
+            double xInterval = (Math.Abs(xStartInput) + Math.Abs(xEndInput)) / 80;
+            //Console.WriteLine(xInterval);
+
+            for (imagCoord = yStartInput; imagCoord >= yEndInput; imagCoord -= yInterval)
+            {
+                for (realCoord = xStartInput; realCoord <= xEndInput; realCoord += xInterval)
+                {
+                    iterations = 0;
+                    realTemp = realCoord;
+                    imagTemp = imagCoord;
+                    arg = (realCoord * realCoord) + (imagCoord * imagCoord);
+                    while ((arg < 4) && (iterations < 40))
+                    {
+                        realTemp2 = (realTemp * realTemp) - (imagTemp * imagTemp)
+                           - realCoord;
+                        imagTemp = (2 * realTemp * imagTemp) - imagCoord;
+                        realTemp = realTemp2;
+                        arg = (realTemp * realTemp) + (imagTemp * imagTemp);
+                        iterations += 1;
+                    }
+                    switch (iterations % 4)
+                    {
+                        case 0:
+                            Console.Write(".");
+                            break;
+                        case 1:
+                            Console.Write("o");
+                            break;
+                        case 2:
+                            Console.Write("O");
+                            break;
+                        case 3:
+                            Console.Write("@");
+                            break;
+                    }
+                }
+                Console.Write("\n");
+            }
+        }
+    }
+}
