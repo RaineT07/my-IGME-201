0a027bb554fb1b7db62238aaf65e680796a66da3
Raine Taber <srt1849@rit.edu>
2022-01-31 15:31:34 -0500

    pe8 question 8

---
+++ PE-8-9/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE_8_9
+{
+    //this program takes any string of words and puts quotation marks around it
+    //author: Raine Taber
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //asking for user input
+            Console.Write("Please enter your input: ");
+            string userInput = Console.ReadLine();
+            //userInput is converted into an array of words
+            string[] words = userInput.Split(' ');
+            string result = "";
+            //using a constant string quote with the " character
+            const string mark = "\"";
+            foreach (string word in words)
+            {
+                result = result + " " + mark + word + mark;
+            }
+            result.Trim();
+            Console.Write(result);
+        }
+    }
+}
