e740bceeea93ce874f3a9c5e245389fd0e840a60
Raine Taber <srt1849@rit.edu>
2022-02-01 18:53:00 -0500

    PE8 question 5

--- PE-8-5/Program.cs
+++ PE-8-5/Program.cs
@@ -6,30 +6,34 @@ using System.Threading.Tasks;
 
 namespace PE_8_5
 {
+    //this program calculates the value of z, and stores x, y, and z in an array
+    //Author: Raine Taber
     class Program
     {
         static void Main(string[] args)
         {
-            double[,,] form = new double[20,50,3];
+            //initializing the array, array indexes, and values to be stored in said array
+            double[,,] form = new double[20, 50, 3];
             int posX = 0;
             int posY = 0;
             double x = 0;
             double y = 0;
             double z = 0;
+            //loop calculates the z value, and stores all data in the proper place
             for (x = -1; x <= 1; x = +0.1, posX++)
             {
                 x = Math.Round(x, 1);
-               for(y =1; y<=4; y=+0.1,++posY )
+                for (y = 1; y <= 4; y = +0.1, ++posY)
                 {
                     y = Math.Round(y, 1);
                     z = ((3 * Math.Pow(y, 3)) + (2 * x) - 1);
-                    z=Math.Round(z,2);
+                    z = Math.Round(z, 2);
                     form[posX, posY, 0] = x;
                     form[posX, posY, 1] = y;
                     form[posX, posY, 2] = z;
                 }
             }
-            
+
         }
     }
 }
