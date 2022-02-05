456396ce7a8796651eeaf0006b0549dc0f2442ab
Raine Taber <srt1849@rit.edu>
2022-02-05 13:42:22 -0500

    PE9 question 3

---
+++ PE9-3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE9_3
+{
+    class Program
+    {
+        delegate string InputReader();
+
+        static string GetInput()
+        {
+            string input = Console.ReadLine();
+            return input;
+        }
+        static void Main(string[] args)
+        {
+            InputReader userInput;
+            userInput = new InputReader(GetInput);
+            string sName = userInput();
+        }
+    }
+}
