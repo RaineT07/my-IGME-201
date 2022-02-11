2d61d3cba897d49a5b4b2c30025073ef81dd5956
Raine Taber <srt1849@rit.edu>
2022-02-11 18:16:19 -0500

    Unit test 1 question 12

---
+++ Unit test 1-12/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unit_test_1_12
+{
+    class Program
+    {
+        //this program prompts the user for their name, and increases their salary if their name is the same as the Author's name
+        //Author: Raine Taber
+        static void Main(string[] args)
+        {
+            //variable initialization
+            string sName;
+            double dSalary = 30000;
+            //Console.WriteLine(dSalary); debug line
+            
+            //prompt user for their name
+            Console.Write("what is your name? ");
+            sName = Console.ReadLine();
+            //if statement calls the giveRaise method, and then 
+            //outputs wether or not the user got a raise.
+            if(GiveRaise(sName, ref dSalary))
+            {
+                Console.WriteLine("Congratulations " + sName + ", you have earned a raise! Your new salary is: $" + dSalary);
+            }
+            else
+            {
+                Console.WriteLine("Tough luck.");
+            }
+
+        }
+        
+        //GiveRaise: checks if the first parameter, the name, is the same as the Author's name (Raine Taber) and gives them a raise if so, before returning true or false based on
+        //if the user's name is the same.
+        static bool GiveRaise(string name, ref double salary)
+        {
+            if(name.Equals("Raine Taber"))
+            {
+                salary += 19999.99;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
