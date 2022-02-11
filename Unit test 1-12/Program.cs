33c4989d8eff76d648699fddb5ee99b3b157734c
Raine Taber <srt1849@rit.edu>
2022-02-11 18:40:55 -0500

    Unit test 1 question 12

--- Unit test 1-12/Program.cs
+++ Unit test 1-12/Program.cs
@@ -12,33 +12,22 @@ namespace Unit_test_1_12
         //Author: Raine Taber
 
 
-        //create struct employee and constructor
-        public struct employee
-        {
-            public string sName;
-            public double dSalary;
-            public employee(string name, double salary)
-            {
-                this.sName = name;
-                this.dSalary = salary;
-            }
-        }
+
 
         static void Main(string[] args)
         {
             //prompt the user for their name
             Console.Write("what is your name? ");
-            string tempName = Console.ReadLine();
+            string sName = Console.ReadLine();
+            double dSalary = 30000;
 
-            //create an employee structure with the user's name and 30000 as the salary
-            employee newEmploy = new employee(tempName, 30000);
-                        
+
             //if statement calls the giveRaise method, and then 
             //outputs wether or not the user got a raise.
 
-            if(GiveRaise(ref newEmploy))
+            if (GiveRaise(sName, ref dSalary))
             {
-                Console.WriteLine("Congratulations " + newEmploy.sName + ", you have earned a raise! Your new salary is: $" + newEmploy.dSalary);
+                Console.WriteLine("Congratulations " + sName + ", you have earned a raise! Your new salary is: $" + dSalary);
             }
             else
             {
@@ -46,14 +35,14 @@ namespace Unit_test_1_12
             }
 
         }
-        
+
         //GiveRaise: checks if the employee structure's sName is the same as the Author's name (Raine Taber) and gives them a raise if so,
         //before returning true or false based on if the user's name is the same.
-        static bool GiveRaise(ref employee e)
+        static bool GiveRaise(string name, ref double salary)
         {
-           if(e.sName.Equals("Raine Taber"))
+            if (name.Equals("Raine Taber"))
             {
-                e.dSalary += 19999.99;
+                salary += 19999.99;
                 return true;
             }
             else
