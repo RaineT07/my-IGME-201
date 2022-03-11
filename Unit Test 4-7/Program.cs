3e094ac6729bf05f5643b9fe8662f73619106e67
Raine Taber <srt1849@rit.edu>
2022-03-11 15:56:53 -0500

    Unit test 2 questions 4-7

---
+++ Unit Test 4-7/Program.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Author:Raine Taber
+//document for Unit test #2, questions 4-7
+
+namespace Unit_Test_4_7
+{
+    //question 4: convert schUML to C#
+    public abstract class Phone
+    {
+        private string phoneNumber;
+        public string address;
+        public string PhoneNumber;
+        public abstract void Connect();
+        public abstract void Disconnect();
+    }
+
+    public interface IPhoneInterface
+    {
+        void Answer();
+        void MakeCall();
+        void HangUp();
+    }
+
+    public class RotaryPhone : Phone, IPhoneInterface
+    {
+        public void Answer()
+        {
+
+        }
+        public void MakeCall()
+        {
+
+        }
+        public void HangUp()
+        {
+
+        }
+        public override void Connect()
+        {
+
+        }
+        public override void Disconnect()
+        {
+            
+        }
+    }
+
+    public class PushButtonPhone : Phone, IPhoneInterface
+    {
+        public void Answer()
+        {
+
+        }
+        public void MakeCall()
+        {
+
+        }
+        public void HangUp()
+        {
+
+        }
+        public override void Connect()
+        {
+
+        }
+        public override void Disconnect()
+        {
+
+        }
+    }
+
+    public class PhoneBooth : PushButtonPhone
+    {
+        private bool superMan;
+        public double costPerCall;
+        public bool phoneBook;
+        public void OpenDoor()
+        {
+
+        }
+        public void CloseDoor()
+        {
+
+        }
+    }
+
+    public class Tardis:RotaryPhone
+    {
+        private bool sonicScrewdriver;
+        private byte whichDrWho;
+        private string femaleSideKick;
+        public double exteriorSurfaceArea;
+        public double interiorVolume;
+
+        public byte WhichDrWho{ get { return whichDrWho; } }
+        public string FemaleSideKick { get { return femaleSideKick; } }
+        public void TimeTravel()
+        {
+
+        }
+
+        //question 5: overload boolean operators
+        //comparison operators are comparing tardis objects based on their whichDrWho value, in which case
+        //they work normally, except for when the 10th doctor is involved, in which case the 10th doctor
+        //is GREATER than every other doctor. this creates some fascinating logic
+        public static bool operator == (Tardis tar1, Tardis tar2)
+        {
+            return tar1.whichDrWho.Equals(tar2.whichDrWho);
+        }
+        public static bool operator !=(Tardis tar1, Tardis tar2)
+        {
+            return tar1.whichDrWho.Equals(tar2.whichDrWho);
+        }
+        public static bool operator <(Tardis tar1, Tardis tar2)
+        {
+            if (tar1.whichDrWho.Equals(10) && tar2.whichDrWho.CompareTo(tar1.whichDrWho)==0)
+            {
+                return false;
+            }
+            else if(tar2.whichDrWho.Equals(10))
+            {
+                return true;
+            }
+            else if(tar1.whichDrWho.CompareTo(tar2.whichDrWho)!=-1 || tar1.whichDrWho.Equals(10))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        public static bool operator >(Tardis tar1, Tardis tar2)
+        {
+            if(tar1.whichDrWho.Equals(10)&&tar2.whichDrWho.CompareTo(tar1.whichDrWho)==0)
+            {
+                return false;
+            }
+            else if(tar1.whichDrWho.Equals(10))
+            {
+                return true;
+            }
+            else if(tar1.whichDrWho.CompareTo(tar2.whichDrWho)!=1||tar2.whichDrWho.Equals(10))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public static bool operator <=(Tardis tar1, Tardis tar2)
+        {
+            if(tar2.whichDrWho.Equals(10))
+            {
+                return true;
+            }
+            else if(tar1.whichDrWho.CompareTo(tar2.whichDrWho)==1 || tar1.whichDrWho.Equals(10))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+
+        }
+        public static bool operator >=(Tardis tar1, Tardis tar2)
+        {
+            if (tar1.whichDrWho.Equals(10))
+            {
+                return true;
+            }
+            else if (tar1.whichDrWho.CompareTo(tar2.whichDrWho) == -1 || tar2.whichDrWho.Equals(10))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+
+
+
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tardis dis = new Tardis();
+            PhoneBooth booth = new PhoneBooth();
+            UsePhone(dis);
+            UsePhone(booth);
+        //UsePhone("yussss");
+        }
+
+        //question 6: UsePhone
+        public static void UsePhone(object obj)
+        {
+            //establish ref variable
+            IPhoneInterface iphone = null;
+            try
+            {
+                //try to cast obj as an IPhoneInterface object, it'll tell you if the obj is
+                //inheriting IPhoneInterface
+                //proceed to set ref variable to obj casted properly, and call IPhoneInterface methods.
+                iphone = (IPhoneInterface)obj;
+                iphone.MakeCall();
+                iphone.HangUp();
+            }
+            catch
+            {
+                Console.WriteLine("not an object inheriting IPhoneInterface");
+            }
+
+            //question 7: add functionality within this method for phoneboothes and tardis' in particular.
+
+            //check if obj is a PhoneBooth object.
+            //proceed to cast obj as a PhoneBooth object.
+            //call phonebooth object using the myBooth ref variable.
+            if (obj.GetType() == typeof(PhoneBooth))
+            {
+                PhoneBooth myBooth = null;
+                myBooth = (PhoneBooth)obj;
+                myBooth.OpenDoor();
+            }
+            else if(obj.GetType()==typeof(Tardis))
+            {
+                Tardis SciFi = null;
+                SciFi = (Tardis)obj;
+                SciFi.TimeTravel();
+            }
+            else
+            {
+                Console.WriteLine("do you even have a phone?");
+            }
+        }
+    }
+}
---
+++ testing code for unit test/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testing_code_for_unit_test
+{
+    public sealed class Circus
+    {
+        public string name;
+    }
+
+
+    static class Program
+    {
+        static void Main()
+        {
+
+            SortedList<(double, double, double), double> formulaList = new SortedList<(double, double, double), double>();
+
+            for(double w = -2; w<=0; w+=0.2)
+            {
+                w = Math.Round(w, 1);
+                for(double x=0; x<= 4; x+=0.1)
+                {
+                    x = Math.Round(x, 1);
+                    for(double y=-1; y<=1; y+=0.1)
+                    {
+                        y = Math.Round(y, 1);
+                        double z = ((4 * Math.Pow(y, 3)) + (2 * Math.Pow(x, 2)) - (8 * w) + 7);
+                        z = Math.Round(z, 3);
+                        formulaList[(w, x, y)] = z;
+                    }
+                }
+            }
+        }
+    }
+
+}
