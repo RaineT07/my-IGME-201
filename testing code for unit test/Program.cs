966dc163992085b1c193097fa85c1600ae1d7ebf
Raine Taber <srt1849@rit.edu>
2022-03-11 20:54:24 -0500

    Unit test 2 quetsion 14

--- testing code for unit test/Program.cs
+++ testing code for unit test/Program.cs
@@ -6,34 +6,19 @@ using System.Threading.Tasks;
 
 namespace testing_code_for_unit_test
 {
-    public sealed class Circus
-    {
-        public string name;
-    }
-
-
+    
     static class Program
     {
-        static void Main()
+        
+        static void Main(string[] args)
         {
-
-            SortedList<(double, double, double), double> formulaList = new SortedList<(double, double, double), double>();
-
-            for(double w = -2; w<=0; w+=0.2)
+            SortedList<string, DateTime> friendBirthday = new SortedList<string, DateTime>();
+            friendBirthday.Add("dave", new DateTime(2022, 7, 14));
+            foreach(KeyValuePair<string, DateTime> events in friendBirthday)
             {
-                w = Math.Round(w, 1);
-                for(double x=0; x<= 4; x+=0.1)
-                {
-                    x = Math.Round(x, 1);
-                    for(double y=-1; y<=1; y+=0.1)
-                    {
-                        y = Math.Round(y, 1);
-                        double z = ((4 * Math.Pow(y, 3)) + (2 * Math.Pow(x, 2)) - (8 * w) + 7);
-                        z = Math.Round(z, 3);
-                        formulaList[(w, x, y)] = z;
-                    }
-                }
+                Console.WriteLine($"{events.Value.Month}/{events.Value.Day}/{events.Value.Year}");
             }
+            Console.WriteLine("end");
         }
     }
 
